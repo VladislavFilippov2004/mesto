b180310867b563f77bafe75b81cf6a30dd544e99
VladislavFilippov2004 <vvf0807@gmail.com>
2020-10-15 02:59:00 +0300

    Коммит 2

--- script/script.js
+++ script/script.js
@@ -1,42 +1,37 @@
-let editButton = document.querySelector('.profile__image');
+let editButton = document.querySelector('.profile__edit-button');
+let Popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__form_icon-close');
+let formElement = document.querySelector('.popup__form');
 
 function popupOpen() {
-let Popup = document.querySelector('.popup');
 Popup.classList.add('popup_opened');
-let nameInput = document.querySelector('#nameInput');
-let jobInput = document.querySelector('#jobInput');
+let nameInput = document.querySelector('.popup__form_input-name');
+let jobInput = document.querySelector('.popup__form_input-job');
 nameInput.value = document.querySelector('.profile__name').textContent;
 jobInput.value = document.querySelector('.profile__job').textContent;
 }
 
-editButton.addEventListener("click", popupOpen);
-
-let closeButton = document.querySelector('.form__icon-close');
-
 function popupClose() {
-    let Popup = document.querySelector('.popup');
     Popup.classList.remove('popup_opened');
 }
 
-closeButton.addEventListener("click", popupClose);
-
-let formElement = document.querySelector('.form__button-save');
-
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-
+    
     let nameInput = document.querySelector('#nameInput');
     let jobInput = document.querySelector('#jobInput');
-       
+    
     let profileTitle = document.querySelector('.profile__name');
     let profileSubtitle = document.querySelector('.profile__job');
-
+    
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
     popupClose();
 }
 
-formElement.addEventListener('click', formSubmitHandler);
+editButton.addEventListener("click", popupOpen);
+closeButton.addEventListener("click", popupClose);
+formElement.addEventListener('submit', formSubmitHandler);
 
 
 
