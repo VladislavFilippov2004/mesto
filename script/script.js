82311a2431cc5568ce29fb36a0176f69d2b5f5bf
VladislavFilippov2004 <vvf0807@gmail.com>
2020-10-17 01:04:12 +0300

    Коммит 3

--- script/script.js
+++ script/script.js
@@ -1,32 +1,27 @@
 let editButton = document.querySelector('.profile__edit-button');
-let Popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__form_icon-close');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__icon-close');
 let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
 
 function popupOpen() {
-Popup.classList.add('popup_opened');
-let nameInput = document.querySelector('.popup__form_input-name');
-let jobInput = document.querySelector('.popup__form_input-job');
+popup.classList.add('popup_opened');
 nameInput.value = document.querySelector('.profile__name').textContent;
 jobInput.value = document.querySelector('.profile__job').textContent;
 }
 
 function popupClose() {
-    Popup.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    
-    let nameInput = document.querySelector('#nameInput');
-    let jobInput = document.querySelector('#jobInput');
-    
+    evt.preventDefault();    
     let profileTitle = document.querySelector('.profile__name');
     let profileSubtitle = document.querySelector('.profile__job');
-    
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    popupClose();
+    popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener("click", popupOpen);
