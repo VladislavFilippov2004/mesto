53707111133b4071fa56d0c515ce7b6a26d3d66c
VladislavFilippov2004 <vvf0807@gmail.com>
2020-10-18 18:20:55 +0300

    Коммит 4

--- script/script.js
+++ script/script.js
@@ -2,13 +2,16 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__icon-close');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+let profileTitle = document.querySelector('.profile__name');
+let profileSubtitle = document.querySelector('.profile__job');
+
 
 function popupOpen() {
 popup.classList.add('popup_opened');
-nameInput.value = document.querySelector('.profile__name').textContent;
-jobInput.value = document.querySelector('.profile__job').textContent;
+nameInput.value = profileTitle.textContent;
+jobInput.value = profileSubtitle.textContent;
 }
 
 function popupClose() {
@@ -17,11 +20,9 @@ function popupClose() {
 
 function formSubmitHandler (evt) {
     evt.preventDefault();    
-    let profileTitle = document.querySelector('.profile__name');
-    let profileSubtitle = document.querySelector('.profile__job');
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    popupClose();
 }
 
 editButton.addEventListener("click", popupOpen);
